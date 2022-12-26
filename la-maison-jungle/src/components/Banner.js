0f87a48fe8d35993f5932c11990c4545cebb8aac
Benjamin <benjamin.belcic.pro@gmail.com>
2022-12-26 19:20:50 +0100

    3

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,5 +1,11 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
 function Banner() {
-    return <h1>La maison jungle</h1>
+    return <div className='lmj-banner'>
+        <img src={logo} alt='la maison jungle' className='lmj-logo' />
+                <h1>La maison jungle</h1>
+           </div>
 }
 
 export default Banner
