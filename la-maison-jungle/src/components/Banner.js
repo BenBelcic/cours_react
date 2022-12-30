ad2c6ba5968a11f77f0ac727a3c7b5024c8caca2
Benjamin <benjamin.belcic.pro@gmail.com>
2022-12-30 16:29:14 +0100

    add plantItem & careScale

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,11 +1,7 @@
-import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
-function Banner() {
-    return <div className='lmj-banner'>
-        <img src={logo} alt='la maison jungle' className='lmj-logo' />
-                <h1>La maison jungle</h1>
-           </div>
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
